e8b55caaa3b031427c9823309a23ac7ab4aa8e1e
AnbuKannappan <anbufird@gmail.com>
2022-12-20 18:32:25 +0530

    Refactored code

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,11 +1,16 @@
+// module.exports = [
+//   "0x79D903745Fc78C4644aa481d4ECd4665e04De413", //equityToken
+//   "0x31B1B28A40d587A933f04402137Bec76e102A344", //tokenTreasury
+//   1000, //cxoAllocation
+//   800,  //seniorManagerAllocation
+//   400, //otherAllocation
+//   120, //Vesting period in seconds
+//   25, //cxoReleasePercent
+//   25, //seniorManagerPercent
+//   50 //otherPercent
+//   ];
+
 module.exports = [
-  "0x79D903745Fc78C4644aa481d4ECd4665e04De413", //equityToken
-  "0x31B1B28A40d587A933f04402137Bec76e102A344", //tokenTreasury
-  1000, //cxoAllocation
-  800,  //seniorManagerAllocation
-  400, //otherAllocation
-  120, //Vesting period in seconds
-  25, //cxoReleasePercent
-  25, //seniorManagerPercent
-  50 //otherPercent
+  "0xB1B68DFC4eE0A3123B897107aFbF43CEFEE9b0A2", //equityToken
+  "0xBb19939D96ca5cd34ec2919eE9Da3a1b70D7A77C", //tokenTreasury
   ];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,8 +9,8 @@ async function main() {
   
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
-    const Token = await ethers.getContractFactory("VirtualEquityToken");
-    const token = await Token.deploy({gasPrice: 200000000000});
+    const Token = await ethers.getContractFactory("Test");
+    const token = await Token.deploy("0xB1B68DFC4eE0A3123B897107aFbF43CEFEE9b0A2","0xBb19939D96ca5cd34ec2919eE9Da3a1b70D7A77C",{gasPrice: 200000000000});
     console.log("BavaTestContract address:", token.address); //0x79D903745Fc78C4644aa481d4ECd4665e04De413
 
     //await hre.run("verify:verify", {address: token.address});
